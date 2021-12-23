d3969920bb554bed5682c83ee35093d48bd34e74
owen254 <owenmiash90@gmail.com>
2021-12-23 20:39:09 +0300

    first commit added adminlogin form design

---
+++ TransportAdmin/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import React from "react"
+import Login from './AdminLogin';
+export default class App extends React.Component{
+    render(){
+        return (<div>
+            <Login/>
+        </div>
+            )    }
+}
